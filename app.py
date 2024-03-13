701d7c74feb4e793db43cb96650386ab876b5424
Petar Milivojevic <petar.milivojevic.biz@gmail.com>
2024-03-13 20:58:20 +0100

    app.py updated

--- app.py
+++ app.py
@@ -33,24 +33,34 @@ PROMPT = PromptTemplate(
 
 chain_type_kwargs = {'prompt': PROMPT}
 
-# llm = CTransformers(
-#     model="/model/llama-2-7b-chat.ggmlv3.q4_0.bin",
-#     model_type="llama",
-#     config={'max_new_tokens': 512, 'temperature': 0.8}
-# )
-# print("POCETAK")
+llm = CTransformers(
+    model="model/llama-2-7b.ggmlv3.q2_K.bin",
+    model_type="llama",
+    config={'max_new_tokens': 512, 'temperature': 0.8}
+)
+print("POCETAK")
 
-# qa = RetrievalQA.from_chain_type(
-#     llm=llm,
-#     chain_type="stuff",
-#     retriever=docsearch.as_retriever(search_kwargs={"k": 2}),
-#     return_source_documents=True,
-#     chain_type_kwargs=chain_type_kwargs
-# )
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=docsearch.as_retriever(search_kwargs={"k": 2}),
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs
+)
 
 @app.route("/")
 def index():
     return render_template('chat.html')
 
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    result = qa({"query": input})
+    print("Response: ", result['result'])
+    
+    return str(result['result'])
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port= 8080, debug=True)
